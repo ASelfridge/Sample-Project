5bfdd599dc5eba1531785db6a53d1dc2fbd6da0d
ASelfridge <andrewselfridge@cmail.carleton.ca>
2016-12-09 17:20:09 -0500

    Added delete user function

--- Assignment 6/Source.cpp
+++ Assignment 6/Source.cpp
@@ -26,13 +26,37 @@ void Register(User u[], int userID)
 	u[userID].isRegistered = true;
 }
 
+void deleteUser(User u[], int userID)
+{
+	int temp = 0;
+	for (int i = 0; i < 20; i++)
+	{
+		if (userID == u[i].id)
+		{
+			u[i].id = -1;
+			for (int j = i; j < 19; j++)
+			{
+				temp = u[j].id;
+				u[j].id = u[j + 1].id;
+				u[j + 1].id = temp;
+			}
+			break;
+		}
+		
+	}
+}
+
 void main()
 {
 	User users[20];
 	for (int i = 0; i < 20; i++)
 	{
 		users[i].id = i;
+		cout << users[i].id << "\n";
 	}
 	Register(users, 3);
 	Register(users, 10);
+	deleteUser(users, 10);
+	deleteUser(users, 5);
+	deleteUser(users, 15);
 }
